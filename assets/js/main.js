491661e5a04df43963065ea2e9bf6d14e28c1426
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-11 18:08:48 +0100

    fizzbuzz complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 const h1Element = document.querySelector('h1')
 
+let i;
+
 for (let i = 100; i > 0; i--) {
     
-    h1Element.insertAdjacentHTML('afterend', `<div class="box center-flex"> ${i} </div>`)
+    if ( i % 3 === 0 && i % 5 === 0 ) {
+
+        h1Element.insertAdjacentHTML('afterend', `<div class="box fizzBuzz"> FIZZBUZZ </div>`)
+    } else if ( i % 3 === 0 ) {
+        h1Element.insertAdjacentHTML('afterend', `<div class="box fizz"> Fizz </div>`)
+    }  else if ( i % 5 === 0 ) {
+        h1Element.insertAdjacentHTML('afterend', `<div class="box buzz"> buzz </div>`)
+    } else {
+        h1Element.insertAdjacentHTML('afterend', `<div class="box"> ${i} </div>`)
+    }
+    
+    
+    
 };
