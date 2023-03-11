bb2c396f46b248f139a611b29273aeb1331d9f2f
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-11 19:10:44 +0100

    ramove the superfluous

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,14 @@
 const h1Element = document.querySelector('h1')
 
-let i;
-
 for (let i = 100; i > 0; i--) {
     
     if ( i % 3 === 0 && i % 5 === 0 ) {
-
         h1Element.insertAdjacentHTML('afterend', `<div class="box fizzBuzz"> FIZZBUZZ </div>`)
     } else if ( i % 3 === 0 ) {
         h1Element.insertAdjacentHTML('afterend', `<div class="box fizz"> Fizz </div>`)
     }  else if ( i % 5 === 0 ) {
-        h1Element.insertAdjacentHTML('afterend', `<div class="box buzz"> buzz </div>`)
+        h1Element.insertAdjacentHTML('afterend', `<div class="box buzz"> Buzz </div>`)
     } else {
         h1Element.insertAdjacentHTML('afterend', `<div class="box"> ${i} </div>`)
-    }
-    
-    
-    
+    } 
 };
